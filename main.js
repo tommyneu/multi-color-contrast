f28024554e4188b842d3414bfa4cd06ca607655c
Thomas Neumann <thomas.neumann@bps101.net>
2022-02-28 16:16:39 -0600

    changed sizes, moved code to own functions, and added popup window for example

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import * as c from "./canvas.js"
 import * as dom from "./dom.js"
-import {eventListener as listen, startUp as start, bind, animate, forAll} from "./listeners.js"
+import {eventListener as listen, startUp as start, bind, animate, forAll, clamp} from "./listeners.js"
 
 const circle = {
     stroke: "white",
@@ -9,38 +9,19 @@ const circle = {
 
 let rects = []
 
-listen("window", "resize", c.fitScreen)
+listen("window", "resize", (e) => {
+    c.fitScreen()
+    document.querySelectorAll("div").forEach((elem) => {
+        elem.remove()
+    })
+})
 
 listen("#add_color", "click", (e) =>{
     if(rects.length >= 6){
         return
     }
-    let temp = {
-        stroke: "white",
-        centered: true,
-        lineWidth: 2,
-        rotation: 0,
-        currentRotation: Math.floor(Math.random() * 360),
-        drawFuncs: []
-    }
-    bind(temp, 'size', () => c.divide('min',  10))
-    bind(temp, 'rotation', () => temp.currentRotation++)
-
-    temp.input = dom.createElementAtPos("input", 0, 0)
-    temp.input.type = "color"
-    temp.input.value = c.randomColor()
-
-    bind(temp, 'fill', () => temp.input.value)
-
-    temp.drawFuncs.push((args) => {
-        dom.moveElementToPos(
-            args.input, 
-            args.x - (dom.getWidth(args.input)/2), 
-            args.y - (dom.getHeight(args.input)/2)
-        )
-    })
-
-    rects.push(temp)
+    
+    rects.push(createRect())
 })
 
 listen("canvas", "click", (e) => {
@@ -49,15 +30,12 @@ listen("canvas", "click", (e) => {
         y: e.clientY
     }
 
-    rects.forEach((rect, index) => {
+    rects.forEach((rect) => {
         if(c.isPointInsideRect(rect, point)){
-            rect.input.remove()
-            rects.splice(index, 1)
+            createExample(rect)
         }
     })
 })
-
-
 
 start(() => {
     bind(circle, 'x', () => c.divide('width', 2))
@@ -71,28 +49,38 @@ start(() => {
 
 function draw(){
     c.distributeAroundCircle(circle, rects)
-    
-    c.background("black")
-    c.drawCircle(circle)
 
     const lines = c.getLinesToConnectAll(
         rects, 
         rects, 
         {
             stroke: "white",
-            lineWidth: 3,
+            lineWidth: 2,
             drawFuncs: [
                 (args) => {
                     let theta = Math.atan((args.y2 - args.y1) / (args.x2 - args.x1))
                     let length = c.getDist(args.x1, args.y1, args.x2, args.y2)
+                    let ratio = getColorContrastRatio(args.originalArgs[0].fill, args.originalArgs[1].fill).toFixed(2)
                     c.drawText({
-                        text: getColorContrastRatio(args.originalArgs[0].fill, args.originalArgs[1].fill).toFixed(2),
+                        text: ratio,
                         x: (args.x1 + args.x2) / 2,
                         y: (args.y1 + args.y2) / 2,
                         piRotation: theta,
-                        xRotationOffset: (length / 2) - c.divide('min', 7),
+                        xRotationOffset: (length / 2) - (args.originalArgs[0].size * 0.95),
                         xCentered: true,
-                        height: c.divide('min',  20),
+                        height: args.originalArgs[0].size / 4,
+                        width: args.originalArgs[0].size / 4,
+                        fill: "white"
+                    })
+                    c.drawText({
+                        text: ratio,
+                        x: (args.x1 + args.x2) / 2,
+                        y: (args.y1 + args.y2) / 2,
+                        piRotation: theta,
+                        xRotationOffset: -1 * ((length / 2) - (args.originalArgs[0].size *  0.95)),
+                        xCentered: true,
+                        height: args.originalArgs[0].size / 4,
+                        width: args.originalArgs[0].size / 4,
                         fill: "white"
                     })
                 }
@@ -100,6 +88,8 @@ function draw(){
         }
     )
 
+    c.background("black")
+    c.drawCircle(circle)
     forAll(lines, c.drawLine)
     forAll(rects, c.drawRect)
 }
@@ -146,4 +136,93 @@ function getLuminance(hexColor){
     }
 
     return (0.2126 * R) + (0.7152 * G) + (0.0722 * B)
+}
+
+function createRect(){
+    let temp = {
+        id: Math.floor(Math.random() * 1000000),
+        stroke: "white",
+        centered: true,
+        lineWidth: 2,
+        drawFuncs: [],
+        elements: {}
+    }
+    bind(temp, 'size', () => clamp(c.divide('min', 8), {min: 60}))
+
+    temp.elements.input = dom.createElementAtPos("input", 0, 0, (elem) => {
+        elem.type = "color"
+        elem.value = c.randomColor()
+    })
+    bind(temp, 'fill', () => temp.elements.input.value)
+
+    temp.elements.deleteButton = dom.createElementAtPos("button", 0, 0, (elem) => {
+        elem.innerHTML = "❌"
+        elem.dataset.index = temp.id
+    })
+    listen(temp.elements.deleteButton, "click", (e)=>{
+        for(const singleElement in temp.elements){
+            temp.elements[singleElement].remove()
+        }
+        rects.forEach((rect, index) => {
+            if(rect.id == e.target.dataset.index){
+                rects.splice(index, 1)
+            }
+        })
+    })
+
+    temp.elements.editButton = dom.createElementAtPos("button", 0, 0, (elem) => {
+        elem.innerHTML = "✏️"
+        elem.dataset.index = temp.id
+    })
+
+    listen(temp.elements.editButton, "click", (e)=>{
+        temp.elements.input.click()
+    })
+
+
+    temp.drawFuncs.push((args) => {
+        dom.moveElementToPos(
+            args.elements.deleteButton, 
+            args.x + (args.size / 2) - dom.getWidth(args.elements.deleteButton), 
+            args.y - (args.size / 2)
+        )
+        dom.moveElementToPos(
+            args.elements.editButton, 
+            args.x + (args.size / 2) - dom.getWidth(args.elements.editButton), 
+            args.y + (args.size / 2) - dom.getHeight(args.elements.editButton)
+        )
+    })
+
+    return temp
+}
+
+function createExample(clickedRect){
+    document.querySelectorAll("div").forEach((elem) => {
+        elem.remove()
+    })
+
+    let elem = dom.createElementAtPos(
+        "div", 
+        c.divide("width", 2) - c.divide("min", 2), 
+        c.divide("height", 2) - c.divide("min", 2), 
+        (element) => {
+            element.innerHTML = "<button id='close_example'>❌</button><input id='example' value='Example Text' />"
+            rects.forEach((rect) => {
+                if(rect.id != clickedRect.id){
+                    element.innerHTML += `<p style='color: ${rect.fill}; font-size: 10vmin;'> Example Text </p>`
+                }
+            })
+            element.style.backgroundColor = clickedRect.fill
+            element.style.width = c.divide("min") + "px"
+            element.style.height = c.divide("min") + "px"
+            element.style.overflowY = "auto"
+            element.style.overflowX = "hidden"
+        }
+    )
+
+    listen('#close_example', "click", (e) => {
+        document.querySelectorAll("div").forEach((elem) => {
+            elem.remove()
+        })
+    })
 }
