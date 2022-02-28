fa6734e94450a7c887da2ac317b4b2df6a4b2d4a
Thomas Neumann <thomas.neumann@bps101.net>
2022-02-28 16:17:26 -0600

    added function callback when creating element

--- dom.js
+++ dom.js
@@ -1,10 +1,13 @@
-export function createElementAtPos(tag, x, y){
+export function createElementAtPos(tag, x, y, createFuncs){
     let element = document.createElement(tag)
     element.style.position = "fixed"
     element.style.top = y + "px"
     element.style.left = x + "px"
 
     document.querySelector("body").append(element)
+
+    createFuncs(element)
+
     return element
 }
 
