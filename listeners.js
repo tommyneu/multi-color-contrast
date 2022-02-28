0f61e53d2c88f83aadbed5e6620350b464d86a31
Thomas Neumann <thomas.neumann@bps101.net>
2022-02-28 16:17:01 -0600

    added clamp function and passing elements into listen

--- listeners.js
+++ listeners.js
@@ -9,19 +9,28 @@ function looping(){
 export function eventListener(query, event, func){
 
     // validate inputs
-    if(typeof query != "string"){
+    if(typeof query != "string" && !(query instanceof Element || query instanceof HTMLDocument )){
         console.error("Invalid value for query", query)
+        return
     }
     if(typeof event != "string"){
         console.error("Invalid value for event", event)
+        return
     }
     if(typeof func != "function"){
         console.error("Invalid value for func", func)
+        return
     }
 
     // window events
     if(query == 'window'){
         window.addEventListener(event, func)
+        return
+    }
+
+    if(query instanceof Element || query instanceof HTMLDocument){
+        query.addEventListener(event, func)
+        return
     }
 
     // query events
@@ -54,4 +63,17 @@ export function bind(objectToBind, propToBind, func){
         }
     })
     return func()
+}
+
+export function clamp(value, args){
+    if(args.min && args.max){
+        return Math.min(Math.max(value, args.min), args.max)
+    }
+    if(args.min){
+        return Math.max(value, args.min)
+    }
+    if(args.max){
+        return Math.min(value, args.max)
+    }
+    return value
 }
